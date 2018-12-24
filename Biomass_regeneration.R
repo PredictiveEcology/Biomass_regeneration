27237b3382945f1d2ecefb6e83d19104cd76ba9f
Eliot McIntire <eliotmcintire@gmail.com>
2018-12-24 12:03:13 -0800

    SpaDES.core version minimum

--- Biomass_regeneration.R
+++ Biomass_regeneration.R
@@ -8,7 +8,7 @@ defineModule(sim, list(
   keywords = c("biomass regeneration", "LandR", "disturbance", "mortality", "vegetation succession", "vegetation model"),
   authors = person("Ceres", "Barros", email = "cbarros@mail.ubc.ca", role = c("aut", "cre")),
   childModules = character(0),
-  version = numeric_version("0.0.1"),
+  version = list(SpaDES.core = "0.2.3.9009", Biomass_regeneration = "0.1.0"),
   spatialExtent = raster::extent(rep(NA_real_, 4)),
   timeframe = as.POSIXlt(c(NA, NA)),
   timeunit = "year",
